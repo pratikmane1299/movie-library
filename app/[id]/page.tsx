a5d4773895586dbafed27d5aab03d795bf0521ce
Pratik Mane <pratik.mane@techno-soft.in>
2023-03-21 00:25:10 +0530

    add meta for movie detail page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,18 +1,56 @@
 import { Suspense } from 'react';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { getMovieById, getRecommedationsForMovie } from "@/services/movies";
 import { MovieDetailType } from '@/types/movies';
+import { getMoviePosterPath } from '@/utils';
 
 import MovieCard from '@/components/MovieCard';
 import Casts from '@/components/Casts';
 import MovieSummary from '@/components/MovieSummary';
 import RecommendedMovies from '@/components/RecommendedMovies';
 
+export async function generateMetadata({ params }: { params: { id: number } }): Promise<Metadata | undefined> {
+	const { id } = params;
+  const movie: MovieDetailType = await getMovieById(id);
+
+	if (!movie) {
+		return {
+			title: '404 | Movie Library',
+			description: 'A movie you are looking for does\'nt exists',
+		}
+	}
+
+	const { title, overview, poster_path } = movie; 
+
+	const moviePosterImage = getMoviePosterPath(poster_path);
+
+	return {
+    title: title,
+    description: overview,
+    openGraph: {
+      title: title,
+      description: overview,
+      type: "website",
+      images: [
+        {
+          url: moviePosterImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description: overview,
+      images: [moviePosterImage],
+    },
+  };
+}
+
 export default async function Page({ params }: { params: { id: number } }) {
   const { id } = params;
   const movie: MovieDetailType = await getMovieById(id);
-  // console.log("movie - ", movie);
 
 	if (!movie) return notFound();
 
